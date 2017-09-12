ef4f838a5ae19db626aecb7229cd9e19fdb28163
Your Name <lnmanit@gmail.com>
2017-09-12 22:52:14 +0530

    pair wise swap linkedlist solution

--- src/com/loki/linkedlist/PairWiseSwap.java
+++ src/com/loki/linkedlist/PairWiseSwap.java
@@ -15,7 +15,7 @@ public class PairWiseSwap {
 	}
 
 	public void insert(int data) {
-		if (head == null){
+		if (head == null) {
 			head = new Node(data);
 			return;
 		}
@@ -27,48 +27,26 @@ public class PairWiseSwap {
 
 		temp.next = new Node(data);
 	}
+
+	public void printList() {
 	
-	public void printList(){
-		if(head == null)
-			return;
-		while(head != null){
-			System.out.print(head.data+" ");
-			head = head.next;
+		Node current = head;
+		while (current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
 		}
 		System.out.println();
 	}
-	
-	public void swapElements(){
-		if(head == null || head.next == null){
-			return;
+
+	public void swapElements() {
+		Node temp = head;
+		while (temp != null && temp.next != null) {
+			int t = temp.data;
+			temp.data = temp.next.data;
+			temp.next.data = t;
+
+			temp = temp.next.next;
 		}
-		
-		Node first = head;
-		Node second = head.next;
-		
-		while(first != null && second != null){
-			Node temp = first;
-			first = second;
-			second = first;
-			
-			if(first.next != null && first.next.next != null){
-				first = first.next.next;
-				
-			}
-			
-			if(first.next != null && first.next.next != null){
-				first = first.next.next;
-				
-			}
-			
-			
-			
-			
-		}
-		
-		
-		
-		
 	}
 
 	public static void main(String[] args) {
@@ -79,6 +57,8 @@ public class PairWiseSwap {
 		ps.insert(7);
 		ps.insert(2);
 		ps.insert(9);
+		ps.printList();
+		ps.swapElements();
 		ps.printList();
 
 	}
