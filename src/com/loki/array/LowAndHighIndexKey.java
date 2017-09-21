843ef941f15db420f45b0458230ae4d93938494c
Your Name <lnmanit@gmail.com>
2017-09-21 10:00:03 +0530

    finding low and high index of a key using binary search

--- src/com/loki/array/LowAndHighIndexKey.java
+++ src/com/loki/array/LowAndHighIndexKey.java
@@ -20,21 +20,58 @@ public class LowAndHighIndexKey {
 		System.out.println("most repeated charachter is " + (max + 1));
 	}
 
-	public void findFirstAndLastIndex(int x) {
-		int first = -1, last = -1;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == x) {
-				if (first == -1) {
-					first = i;
-					last = i;
-				} else {
-					last = i;
-				}
-			}
-			}
+	/*
+	 * @Deprecated public void findFirstAndLastIndex(int x) { int first = -1,
+	 * last = -1; for (int i = 0; i < arr.length; i++) { if (arr[i] == x) { if
+	 * (first == -1) { first = i; last = i; } else { last = i; } } }
+	 * 
+	 * System.out.println("first index is => " + first);
+	 * System.out.println("last index is => " + last); }
+	 */
 
-		System.out.println("first index is => " + first);
-		System.out.println("last index is => " + last);
+	public void findFirstAndLastIndex(int x) {
+		int low = 0, high = arr.length - 1;
+		int leftIndex = leftBinarySearch(low, high, x);
+		int rightIndex = rightBinarySearch(low, high, x);
+		if (leftIndex != 1 && rightIndex != -1) {
+			System.out.println("left index is " + leftIndex);
+			System.out.println("right index is " + rightIndex);
+		}
+
+	}
+
+	public int rightBinarySearch(int low, int high, int x) {
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (mid == arr.length - 1 || x < arr[mid + 1] && arr[mid] == x) {
+				return mid;
+			} else if (x < arr[mid]) {
+				high = mid - 1;
+
+			} else {
+				low = mid + 1;
+
+			}
+		}
+		return -1;
+	}
+
+	// { 1, 3, 5, 5, 5, 5, 67, 123, 125 };
+
+	public int leftBinarySearch(int low, int high, int x) {
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (mid == 0 || arr[mid - 1] < x &&  arr[mid] == x) {
+				return mid;
+			} else if (arr[mid] < x) {
+				low = mid + 1;
+
+			} else {
+				high = mid - 1;
+
+			}
+		}
+		return -1;
 	}
 
 	public static void main(String[] args) {
