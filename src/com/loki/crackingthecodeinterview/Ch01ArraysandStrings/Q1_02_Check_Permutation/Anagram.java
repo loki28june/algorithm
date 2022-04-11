825c0f0953be417b56d79cff5846b3044078e296
Lokesh Nandanwar <LNMANIT@gmail.com>
2022-04-11 21:14:51 +0530

    Solved 2nd Question of 189 Cracking the code interview

--- src/com/loki/crackingthecodeinterview/Ch01ArraysandStrings/Q1_02_Check_Permutation/Anagram.java
+++ src/com/loki/crackingthecodeinterview/Ch01ArraysandStrings/Q1_02_Check_Permutation/Anagram.java
@@ -1,25 +1,30 @@
 package com.loki.crackingthecodeinterview.Ch01ArraysandStrings.Q1_02_Check_Permutation;
 
+
 public class Anagram {
     private static boolean isAnagram(String s1, String s2){
         if(s1.length() != s2.length()){
-            return false;
+            return false; // Permutations must be same length
         }
-        // assuming words from 'a' to 'z'
-        int[] chars = new int[128];
-        for(int i=0 ; i<s1.length(); i++){
-            chars[s1.charAt(i)]++;
+        int[] letters = new int[128]; // assumed ASCII
+        for(int i=0;i<s1.length();i++){
+            letters[s1.charAt(i)]++;
         }
-        for(int i=0; i<s2.length() ; i++){
-            chars[s2.charAt(i)]--;
-            if(chars[s2.charAt(i)] < 0){
+        for(int i=0;i<s2.length();i++){
+            letters[s2.charAt(i)]--;
+            if(letters[s2.charAt(i)] < 0){
                 return false;
             }
         }
         return true;
     }
     public static void main(String[] args){
-        System.out.println(isAnagram("banana", "nanaba"));
-        System.out.println(isAnagram("sukkon","kasoor"));
+        String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
+        for (String[] pair : pairs) {
+            String word1 = pair[0];
+            String word2 = pair[1];
+            boolean anagram = isAnagram(word1, word2);
+            System.out.println(word1 + ", " + word2 + ": " + anagram);
+        }
     }
 }
