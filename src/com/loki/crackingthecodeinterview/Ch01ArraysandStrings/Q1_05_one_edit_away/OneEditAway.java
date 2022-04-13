0efd51b3d694a028faae1d68521eb06c4f439895
Lokesh Nandanwar <LNMANIT@gmail.com>
2022-04-13 07:33:15 +0530

    Solved 5th question of 189 cracking the code interview

--- src/com/loki/crackingthecodeinterview/Ch01ArraysandStrings/Q1_05_one_edit_away/OneEditAway.java
+++ src/com/loki/crackingthecodeinterview/Ch01ArraysandStrings/Q1_05_one_edit_away/OneEditAway.java
@@ -2,24 +2,23 @@ package com.loki.crackingthecodeinterview.Ch01ArraysandStrings.Q1_05_one_edit_aw
 
 public class OneEditAway {
     private static boolean isOneEditAway(String s1, String s2){
-        if(Math.abs(s1.length() - s2.length()) > 1)
-            return false;
-        int[] table = new int[128];
-        for (int i =0; i <s1.length(); i++){
-            table[s1.charAt(i)]++;
+       if(Math.abs(s1.length() - s2.length()) >1){
+           return false;
+       }
+       int[] letters = new int[128]; // Assumed Ascii
+        for(char c : s1.toCharArray()){
+            letters[c]++;
         }
-        for (int i =0; i <s2.length(); i++){
-            table[s2.charAt(i)]--;
+        for(char c : s2.toCharArray()){
+            letters[c]--;
         }
-        int countOneEdit =0;
-        for (int i =0; i <128; i++){
-            if(table[i] == 0) continue;
-            if(Math.abs(table[i]) > 1){
+        int editCount=0;
+        for(int count : letters){
+            if(editCount > 1){
                 return false;
             }
-            if(Math.abs(table[i]) == 1){
-                countOneEdit ++;
-                if(countOneEdit > 2) return false;
+            if(count > 0){
+                editCount++;
             }
         }
         return true;
