ceb90af2b5b41a3ab8b16ed8019a2d81290727a8
Lokesh Nandanwar <LNMANIT@gmail.com>
2022-04-16 11:38:00 +0530

    Solved 7th question of 189 cracking the code interview

--- src/com/loki/crackingthecodeinterview/Ch01ArraysandStrings/Q1_07_rotate_image/RotateImage.java
+++ src/com/loki/crackingthecodeinterview/Ch01ArraysandStrings/Q1_07_rotate_image/RotateImage.java
@@ -1,43 +1,46 @@
 package com.loki.crackingthecodeinterview.Ch01ArraysandStrings.Q1_07_rotate_image;
 
 public class RotateImage {
-    public static void rotateImage(int[][] image){
-        if(image.length == 0 || image.length != image[0].length) return;
-        int N = image.length;
-        //step 1 transpose matrix
-        for(int i =0; i<N; i++){
-            for(int j=i; j<N;j++){
-                int temp = image[i][j];
-                image[i][j] = image[j][i];
-                image[j][i] = temp;
+    public static void rotateImage(int[][] image) {
+        if (image.length == 0 || image.length != image[0].length) {
+            return; // not a sqare
+        }
+        int n = image.length;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            for (int i = 0; i < right - left; i++) {
+                int top = left, bottom = right;
+                int topLeft = image[top][left + i]; // save the top left
+
+                image[top][left + i] = image[bottom - i][left];
+                image[bottom - i][left] = image[bottom][right - i];
+                image[bottom][right - i] = image[top + i][right];
+                image[top + i][right] = topLeft;
             }
+            left++;
+            right--;
         }
 
-        //step 2 flip the row elements horizontally
-        for(int i =0; i<N; i++){
-            for(int j=0; j<N-2;j++){
-                int temp = image[i][j];
-                image[i][j] = image[i][N-1-j];
-                image[i][N-1-j] = temp;
-            }
-        }
     }
 
-    private static void printArray(int[][] image){
-        int N = image.length;
-        for(int i =0; i<N; i++){
-            for(int j=0; j<N;j++){
-              System.out.print(image[i][j] + ",");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args){
-        int[][] n = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    public static void main(String[] args) {
+        int[][] n = {{1, 2, 3, 4}
+                , {5, 6, 7, 8}
+                , {9, 10, 11, 12}
+                , {13, 14, 15, 16}};
         printArray(n);
         rotateImage(n);
         System.out.println();
         printArray(n);
+    }
+
+    private static void printArray(int[][] image) {
+        int N = image.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(image[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
